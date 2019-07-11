e3c6584d9aa48aa99b4ce3d99d24cf9af180ca9a
anthonySegura <ansechaves@gmail.com>
2019-07-10 22:24:30 -0600

    First commit

---
+++ src/api_requests/requests.js
@@ -0,0 +1,251 @@
+async function getProducts() {
+  return {
+    products:
+      [
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }, {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        },
+        {
+          name: 'producto genérico',
+          id: '1',
+          img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+          price: 12345,
+          stock: 25,
+          tax: 0
+        }
+      ]
+  }
+}
+
+async function getProduct(id) {
+  // Consultar al api por la informacion de un producto especifico
+  return {
+    name: 'producto genérico',
+    id: '1',
+    img: 'https://pbs.twimg.com/media/D6T-BpiX4AE3k-l.jpg',
+    price: 12345,
+    stock: 25,
+    tax: 0
+  }
+}
+
+async function getOrders() {
+  return {
+    orders: [
+      {
+        id: 1,
+        address: 'por ahí',
+        email: 'correo@gmail.com',
+        state: {
+          despachado: false,
+          entregado: false
+        }
+      },
+      {
+        id: 1,
+        address: 'por ahí',
+        email: 'correo@gmail.com',
+        state: {
+          despachado: false,
+          entregado: false
+        }
+      },
+      {
+        id: 1,
+        address: 'por ahí',
+        email: 'correo@gmail.com',
+        state: {
+          despachado: false,
+          entregado: false
+        }
+      },
+      {
+        id: 1,
+        address: 'por ahí',
+        email: 'correo@gmail.com',
+        state: {
+          despachado: false,
+          entregado: false
+        }
+      },
+      
+    ]
+  }
+}
+
+export {
+  getProducts,
+  getProduct,
+  getOrders
+};
